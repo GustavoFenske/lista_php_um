ca874617b19d0cca04a7dedb0f4c55a6c28683a5
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-07-08 11:08:53 -0300

    add- iniciei a 5

---
+++ ex_05.php
@@ -0,0 +1,24 @@
+<?php
+
+function analisarTexto($texto){
+
+$quantidadeCaracteres = mb_strlen($texto);
+
+return $quantidadeCaracteres;
+}
+
+
+$texto = "gugupro esteve aqui!" 
+
+
+
+
+
+
+
+
+
+
+?>
+
+
