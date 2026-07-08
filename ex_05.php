17df183525c359db5b05db583227d22a6da0865e
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-07-08 12:32:02 -0300

    add- terminei o ex5

--- ex_05.php
+++ ex_05.php
@@ -1,15 +1,41 @@
 <?php
 
-function analisarTexto($texto){
+function analisarTexto($texto)
+{
 
-$quantidadeCaracteres = mb_strlen($texto);
+    $quantidadeCaracteres = mb_strlen($texto);
+    $palavras = explode(" ", $texto);
+    $palavras = count($palavras);
 
-return $quantidadeCaracteres;
+    $vogais = preg_match_all('/[aeiou]/i', $texto);
+      $consoantes = $quantidadeCaracteres - $vogais;
+
+
+
+
+
+    return [
+        "quantidade" => $quantidadeCaracteres,
+        "palavras" => $palavras,
+        "vogais" => $vogais,
+        "consoantes" => $consoantes
+    ];
+
+
 }
+$texto = "Certa manhã, ao despertar de sonhos intranquilos, Gregor Samsa encontrou-se em sua cama metamorfoseado num inseto monstruosos";
+
+$resultado = analisarTexto($texto);
 
 
-$texto = "gugupro esteve aqui!" 
 
+
+
+echo "seu texto é : " . $texto . "<br>";
+echo "a quantidade de caractere é : " . $resultado['quantidade'] . "<br>";
+echo "a quantidade de palavras é : " . $resultado['palavras'] . "<br>";
+echo "a quantidade de vogais é : " . $resultado['vogais'] . "<br>";
+echo "a quantidade de consoantes é : " . $resultado['consoantes'] . "<br>";
 
 
 
@@ -20,5 +46,3 @@ $texto = "gugupro esteve aqui!"
 
 
 ?>
-
-
