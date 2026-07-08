c6970a8fe0a54555f2efd4862c639d3fb42e04e4
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-07-08 09:29:36 -0300

    add - finalizei o ex03

--- ex_03.php
+++ ex_03.php
@@ -5,11 +5,15 @@ function mascararCpf($cpf){
 
 $cpf = preg_replace("/[^0-9]/", "", $cpf);
 
+$ultimosdigitos = substr($cpf, -4);
+
+return "Seu cpf é : *******" . $ultimosdigitos;
 
 }
-    $cpf = "123.456.789-10";
 
-echo marcararCpf($cpf);
+    echo mascararCpf("123.456.789-10");
+
+
 
 
 
